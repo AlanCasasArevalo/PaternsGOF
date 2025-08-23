392c12f0a40701e7499459e590fff61f88ff190a
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2025-08-23 21:42:33 +0200

    [WIP] Undo command was implemented in the first class

--- PaternsGOF/Command/SetFanSpeedToLowCommand.swift
+++ PaternsGOF/Command/SetFanSpeedToLowCommand.swift
@@ -1,12 +1,29 @@
 class SetFanSpeedToLowCommand: Command {
     private let fan: FanDriver
+    private var previousState: FanDriver.State = .off
+    private var previousSpeed: FanDriver.Speed = .low
     
     init(fan: FanDriver) {
         self.fan = fan
     }
     
     func execute() {
+        storePreviousFanState()
         fan.turnOn()
         fan.set(speed: .low)
+    }
+    
+    func storePreviousFanState() {
+        previousState = fan.isOn() ? .on : .off
+        previousSpeed = fan.getSPeed()
+    }
+    
+    func undo() {
+        if previousState == .on {
+            fan.turnOn()
+        } else {
+            fan.turnOff()
+        }
+        fan.set(speed: previousSpeed)
     }
 }
